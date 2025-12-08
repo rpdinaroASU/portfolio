fb9295ca2eca3d1abda73f6a81a4229318c876d9
Emily Dinaro <emilydinaro@gmail.com>
2025-12-08 12:39:49 -0700

    Implement About Me Page and Contact page Reorganization

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ window.onload = () => {
     const contactPage = document.querySelector("#contact-me");
     document.querySelector("#close-button").addEventListener("click", function () {
         contactPage.style.visibility = "hidden";
-        document.querySelector("body").style.overflow = "visible";
+        document.querySelector("body").style.overflowY = "visible";
+        document.querySelector("#blurrable").style.filter = "none";
     });
     document.querySelector("#contact").addEventListener("click", function () {
         contactPage.style.visibility = "visible";
+        contactPage.display = "flex";
         document.querySelector("body").style.overflow = "hidden";
         document.querySelector("#modal-backdrop").style.display = "flex";
+        document.querySelector("#blurrable").style.filter = "blur(5px)";
     });
     document.querySelector("#github-title").addEventListener("click", function () {
         window.location.assign('https://github.com/rpdinaroASU');
@@ -18,8 +21,9 @@ window.onload = () => {
     });
     document.querySelector("#modal-backdrop").addEventListener("click", function () {
         contactPage.style.visibility = "hidden";
-        document.querySelector("body").style.overflow = "visible";
+        document.querySelector("body").style.overflowY = "visible";
         document.querySelector("#modal-backdrop").style.display = "none";
+        document.querySelector("#blurrable").style.filter =  "none";
     });
     const copyrightElement = document.querySelector("#copyright");
     if (copyrightElement) {
@@ -27,5 +31,17 @@ window.onload = () => {
             copyrightElement.innerHTML = "&copy; 2025 - " + new Date().getFullYear() + " www.emily-dinaro.portfolio - All Rights Reserved.";
         })();
     }
+    document.querySelector("#about").addEventListener("click", function () {
+        const project = document.querySelector("#content");
+        project.style.display = "none";
+        const aboutPage = document.querySelector("#about-me");
+        aboutPage.style.display = "flex";
+    });
+    document.querySelector("#home-page").addEventListener("click", function () {
+        const project = document.querySelector("#content");
+        project.style.display = "flex";
+        const aboutPage = document.querySelector("#about-me");
+        aboutPage.style.display = "none";
+    });
 
 }
