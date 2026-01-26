51f236cbb4b639c912680a0cf3f0ed68b324a1ed
Emily Dinaro <emilydinaro@gmail.com>
2026-01-26 13:07:06 -0700

    Implmented Modal FSM control over project page

--- modules/ModalFSM.js
+++ modules/ModalFSM.js
@@ -6,42 +6,61 @@ export class ModalFSM {
         this.lastState = this.modals.HOME_MODAL;
         this.projectModals = new Set([this.modals.SATELLITE_MODAL, this.modals.GENETIC_ALGORITHM_MODAL, this.modals.LORE_MYTHOLOGY_MODAL]);
         this.contact = false;
+        this.currentProject = this.modals.HOME_MODAL;
     }
     setState(state) {
         if(state.id !== this.modals.CONTACT_MODAL.id) {
+            //If a modal change occurs and the contact modal is active,
+            //Hide the contact modal
             if(this.contact) {
-                this.contact = false;
-                this.updateContactModal();
+                this.updateContactModalDisplay(false);
             }
-            let isLastStateProject = this.projectModals.has(this.lastState);
-            let isNewStateProject = this.projectModals.has(state);
-            if(isNewStateProject) {
-                state.parentElement.style.display = "block";
-                this.projectModals.forEach(modal =>
-                    modal.style.display = (modal.id === state.id) ? "block" : "none");
-            } else if (isLastStateProject) {
-                state.style.display = "initial";
-                this.lastState.parentElement.style.display = "none";
-            } else {
-                state.style.display = "initial";
-            }
-            //Only update the last state if it's not the contact modal
-            this.lastState.style.display = "none";
-            this.lastState = state;
 
+            //If the state is a project modal, show the project modal
+            if(this.projectModals.has(state)){
+                this.updateProjectModalDisplay(state);
+            } else {
+                //If the state is not a project modal,
+                // hide the project modals.
+                if(this.projectModals.has(this.currentProject)) {
+                    this.hideProjectModal();
+                    this.currentProject = state;
+                }
+                //Show the new state
+                this.lastState.style.display = "none";
+                state.style.display = "block";
+                this.lastState = state;
+            }
         } else {
-            this.contact = true;
-            this.updateContactModal();
+            //If the state is the contact modal, show it
+            this.updateContactModalDisplay(true);
         }
 
     }
+
     // When the contact model is selected,
     // blur the background and show the contact modal.
-    // Don't update the last state.
-    updateContactModal() {
+    // When the contact modal is hidden, unblur the background.
+    updateContactModalDisplay(isVisible) {
+        this.contact = isVisible;
         this.blurrableParent.style.filter = this.contact ? "blur(5px)" : "none";
         this.modals.CONTACT_MODAL.style.display = this.contact ? "initial" : "none";
         this.modals.CONTACT_MODAL.style.visibility = this.contact ? "visible" : "hidden";
         this.backdrop.style.display = this.contact ? "flex" : "none";
+    }
+    updateProjectModalDisplay(projectModal) {
+        this.currentProject = projectModal;
+        if (this.projectModals.has(this.currentProject)) {
+            this.lastState.style.display = "none";
+            this.currentProject.parentElement.style.display = "block";
+            this.projectModals.forEach(modal => {
+                modal.style.display = (modal.id === this.currentProject.id) ? "block" : "none";
+            });
+        } else {
+            this.hideProjectModal();
+        }
+    }
+    hideProjectModal() {
+        this.modals.SATELLITE_MODAL.parentElement.style.display = "none";
     }
 }
